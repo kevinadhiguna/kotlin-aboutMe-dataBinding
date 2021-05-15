9aeecd6b84beef6e2d2fc3901747a6b3396b7877
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-15 07:59:43 +0700

    refactor: add addNickname function and set onClickListener

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it) // <- it refers to the done_button, which is the view passed as the argument
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        // Set the text in the nicknameTextView text view to the text that the user entered in the editText, getting it from the text property
+        nicknameTextView.text = editText.text
+
+        // Hide the nickname EditText view by setting the visibility property of editText to View.GONE
+        editText.visibility = View.GONE
+
+        // Hide the DONE button by setting the visibility property to View.GONE
+        view.visibility = View.GONE
+
+        //  Make the nickname TextView view visible by setting its visibility property to View.VISIBLE
+        nicknameTextView.visibility = View.VISIBLE
     }
 }
